10b2c7137e4dd3583045cf5ff0d4b34bfd7edc5c
Jan Tumbokon <dev.jtumbokon@gmail.com>
2020-07-15 21:26:59 -0600

    add next-optimized-image

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
-const path = require("path");
-const withCSS = require("@zeit/next-css");
+const path = require('path');
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
-module.exports = {
-  webpack: (config) => {
-    config.resolve.alias["components"] = path.join(__dirname, "components");
-    config.resolve.alias["public"] = path.join(__dirname, "public");
+module.exports = withPlugins([
+  [optimizedImages, {}],
 
-    return config;
+  // your other plugins here
+  {
+    webpack: config => {
+      config.resolve.alias['components'] = path.join(__dirname, 'components');
+      config.resolve.alias['public'] = path.join(__dirname, 'public');
+
+      return config;
+    },
   },
-};
+]);
